ac28bfde622c3d1ca92e75d35c82713b407b43fe
pix4d_concourse_developers <pix4d_concourse_developers@pix4d.com>
2024-11-29 12:34:10 +0000

    Publish pyopf

--- src/pyopf/ext/pix4d_region_of_interest.py
+++ src/pyopf/ext/pix4d_region_of_interest.py
@@ -11,23 +11,22 @@ version = VersionInfo(1, 0, "draft1")
 
 
 class Pix4DRegionOfInterest(ExtensionItem):
-
     """Definition of a region of interest: a planar polygon with holes and an optional
-    height, defined as a the distance from the plane in the normal direction. All the
+    thickness, defined as a the distance from the plane in the normal direction. All the
     points on the hemispace where the normal lies that project inside the polygon and is at a
-    distance less than the height of the ROI, is considered to be within.
+    distance less than the thickness of the ROI, is considered to be within.
     """
 
     plane: Plane
-    """The height of the ROI volume, defined as a limit distance from the plane in the normal
-    direction. If not specified, the height is assumed to be infinite.
+    thickness: Optional[float]
+    """The thickness of the ROI volume, defined as a limit distance from the plane in the normal
+    direction. If not specified, the thickness is assumed to be infinite.
     """
-    height: Optional[float]
 
     def __init__(
         self,
         plane: Plane,
-        height: Optional[float],
+        thickness: Optional[float],
         pformat: ExtensionFormat = format,
         version: VersionInfo = version,
     ) -> None:
@@ -35,22 +34,22 @@ class Pix4DRegionOfInterest(ExtensionItem):
 
         assert self.format == format
         self.plane = plane
-        self.height = height
+        self.thickness = thickness
 
     @staticmethod
     def from_dict(obj: Any) -> "Pix4DRegionOfInterest":
         base = ExtensionItem.from_dict(obj)
         plane = Plane.from_dict(obj["plane"])
-        height = from_union([from_float, from_none], obj.get("height"))
-        result = Pix4DRegionOfInterest(plane, height, base.format, base.version)
+        thickness = from_union([from_float, from_none], obj.get("thickness"))
+        result = Pix4DRegionOfInterest(plane, thickness, base.format, base.version)
         result._extract_unknown_properties_and_extensions(obj)
         return result
 
     def to_dict(self) -> dict:
         result = super(Pix4DRegionOfInterest, self).to_dict()
         result["plane"] = to_class(Plane, self.plane)
-        if self.height is not None:
-            result["height"] = from_union([to_float, from_none], self.height)
+        if self.thickness is not None:
+            result["thickness"] = from_union([to_float, from_none], self.thickness)
         return result
 
 
